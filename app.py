d4d8cddf13482c9389681ad80f1a4330a0fa5a81
scottnuma <scott.numamoto@gmail.com>
2018-03-20 01:28:09 -0700

    Determine that flask is single threaded

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import datetime
+import datetime, time
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -8,6 +8,21 @@ def hello_world(name=None):
 
 @app.route('/time')
 def get_time():
+    return str(datetime.datetime.now())
+
+@app.route('/wait')
+def wait_a_bit():
+    """Wait 5 seconds before returning the time.
+
+    We can use this function to determine that flask runs in a single
+    thread. Flask cannot service other requests while /wait 
+    completes.
+
+    Concurrent calls of `curl localhost:5000/wait` and then `curl 
+    localhost:5000/time` will both return at the same time after 5
+    seconds.
+    """
+    time.sleep(5)
     return str(datetime.datetime.now())
 
 if __name__ == "__main__":
