7fc9809d24eadd6a8378676f3b43babc227fb6d1
scottnuma <scott.numamoto@gmail.com>
2018-03-25 12:27:04 -0700

    Change comment format

--- student_code.py
+++ student_code.py
@@ -15,24 +15,25 @@ async def drive_fwd():
     await Actions.sleep(3.0)
 
 def teleop_setup():
-    Robot.run(drive_fwd)
+    pass
+    # Robot.run(drive_fwd)
 
 def teleop_main():
     if Robot.is_running(drive_fwd):
         return # wait until it completes
 
-    driving_mode = 1
+    driving_mode = 0
 
     if driving_mode == 0:
-        """Driving straight. """
+        # Driving straight
         Robot.set_value("left_motor", "duty_cycle", 0.7)
         Robot.set_value("right_motor", "duty_cycle", -0.7)
     elif driving_mode == 1:
-        """Tank Drive"""
+        # Tank Drive
         Robot.set_value("left_motor", "duty_cycle", Gamepad.get_value("joystick_left_y"))
         Robot.set_value("right_motor", "duty_cycle", -Gamepad.get_value("joystick_right_y"))
     elif driving_mode == 2:
-        """Arcade Drive"""
+        # Arcade Drive
         turningSpeed =  Gamepad.get_value("joystick_left_x")
         left_y = -Gamepad.get_value("joystick_left_y")
         turningSpeed = turningSpeed * abs(turningSpeed)
