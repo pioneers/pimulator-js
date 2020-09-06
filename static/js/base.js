f184638c3f8ad3bdd99d508df9d7093e692c1106
Charles Hong <charleshong@berkeley.edu>
2020-09-05 18:21:21 -0700

    started conversion; simplified base.js

--- static/js/base.js
+++ static/js/base.js
@@ -1,52 +1,49 @@
-var screenUpdate;
+var simulator;
 
-function req(arg, act) {
-    var xhttp = new XMLHttpRequest();
-    var scaleFactor = 2;
-    if (act) {
-        xhttp.onreadystatechange = function() {
-              if (this.readyState == 4 && this.status == 200) {
-                  var state = JSON.parse(this.responseText);
-                //   console.log("StateX")
-                //   console.log(state.x)
-                  document.getElementById("demo").innerHTML = state.x.toFixed(2) + ", " + state.y.toFixed(2)
-                  var robotRect = document.querySelector("rect")
-                //   console.log("SVG")
-                //   console.log(robotRect)
-                  robotRect.setAttributeNS(null, "x", state.x)
-                  robotRect.setAttributeNS(null, "y", state.y)
-                  var rotateStr = "rotate(" + state.theta + " " + (state.x + 15*scaleFactor) + " " + (state.y + 20*scaleFactor) + ")"
-                //   console.log(rotateStr)
-                  robotRect.setAttribute("transform", rotateStr)
-                // console.log("Adjusted")
-              }
-        };
+function update(state) {
+  /*
+  Update the state (position and direction) of the robot.
+  Example of state: {x:72, y:72, theta:0}
+  */
+  // console.log("StateX")
+  // console.log(state.x)
+  document.getElementById("demo").innerHTML = state.x.toFixed(2) + ", " + state.y.toFixed(2)
+  var robotRect = document.querySelector("rect")
+  // console.log("SVG")
+  // console.log(robotRect)
+  robotRect.setAttributeNS(null, "x", state.x)
+  robotRect.setAttributeNS(null, "y", state.y)
+  var rotateStr = "rotate(" + state.theta + " " + (state.x + 15*scaleFactor) + " " + (state.y + 20*scaleFactor) + ")"
+  // console.log(rotateStr)
+  robotRect.setAttribute("transform", rotateStr)
+  // console.log("Adjusted")
+};
+
+function start(auto=0) {
+    /*
+    Start the robot thread
+    Return if started robot thread
+    */
+    if (simulator != null) {
+        return;
     }
-    xhttp.open("GET", arg, true);
-    xhttp.send();
-}
+    else {        
+        simulator = Simulator();
 
-function update() {
-    req("/state", true)
-}
+        // We utilize a daemon thread to such that the thread exits even if we
+        // do not exit gracefully from __main__
+        // robotWorker = new Worker("{{ url_for('static', filename='js/pimulator.js') }}");
+        // robotWorker = threading.Thread(group=None, target=pimulator.main, args=(stateQueue,auto,stop),
+        //                                name="robot thread", daemon=True)
+        console.log("robot started");
+    }
+};
 
 function stop() {
-    req("/stop", false);
-    clearInterval(screenUpdate);
-}
-
-function start_teleop() {
-    req("/start_teleop", false)
-    screenUpdate = setInterval(update, 50);
-}
-
-function start_auto(){
-  req("/start_auto", false)
-  screenUpdate = setInterval(update, 50)
-}
-
-function send_code(data){
-    console.log(data)
-  req("/stop", false);
-  req("/send_code/" + data, false)
-}
+    /*
+    Stop the robot thread
+    */
+    simulator.stop();
+    simulator = null;
+    update({x:72, y:72, theta:0});
+};
