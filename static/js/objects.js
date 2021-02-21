b5fc98d6a4f0ae7cc94d536ea31231c8930190ea
Charles Hong <31425218+charleshong3@users.noreply.github.com>
2021-02-20 16:07:56 -0800

    Change default tape lines to a rectangle (#61)

--- static/js/objects.js
+++ static/js/objects.js
@@ -1,8 +1,9 @@
 var objects = {
     tapeLinesData: [
-        {x1: 27, y1: 27, x2: 115, y2: 115, color: "green"},
-        {x1: 27, y1: 27, x2: 115, y2: 27, color: "green"},
-        {x1: 115, y1: 27, x2: 115, y2: 115, color: "green"}
+        {x1: 24, y1: 72, x2: 120, y2: 72, color: "green"},
+        {x1: 24, y1: 24, x2: 24, y2: 72, color: "green"},
+        {x1: 24, y1: 24, x2: 120, y2: 24, color: "green"},
+        {x1: 120, y1: 24, x2: 120, y2: 72, color: "green"}
     ],
     wallsData: [
         {x: 0, y: 0, w: 144, h: 2},
