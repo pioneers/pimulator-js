f298e97283cfab36afd380ce75f1a2c5df9c5707
Charles Hong <charleshong@berkeley.edu>
2021-03-29 20:51:56 -0700

    tweaks

--- static/js/base.js
+++ static/js/base.js
@@ -9,11 +9,6 @@ var codeUploaded = false;
 const scaleFactor = 3;
 const canvas = document.getElementById('fieldCanvas');
 const ctx = canvas.getContext('2d');
-var globalRobot = null;
-var globalObjects = null;
-
-// var obstacles = [];
-// var tapelines = [];
 
 // Handle messages from worker
 function onmessage(e) {
@@ -100,7 +95,7 @@ function drawRobot(robot) {
 drawRobot({
     X: Number($("#xpos").val()),
     Y: Number($("#ypos").val()),
-    dir: 0
+    dir: direction
 });
 
 function drawObjs(objs, type) {
@@ -199,7 +194,7 @@ function uploadObjects(){
         let robot = {
             X: Number($("#xpos").val()),
             Y: Number($("#ypos").val()),
-            dir: 0
+            dir: direction
         };
         drawRobot(robot);
     }
