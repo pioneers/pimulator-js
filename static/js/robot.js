412e551057a098dd85de43dc0bae29b42df7e87e
Ryan Chao <chaoryan5@gmail.com>
2021-04-26 21:19:09 -0700

    field not erased if bad field upload

--- static/js/robot.js
+++ static/js/robot.js
@@ -1103,24 +1103,22 @@ this.onmessage = function(e) {
           let objects = f();
           simulator.defineObjs(objects);
 
-          // Draw objects if no active simulation
-          if (simulator.mode == "idle") {
-              simulator.drawObjs();
-          }
-
-          // Objects get redefined right away in teleop mode.
-          // Attached objects get removed, so set to null.
-          if (simulator.mode == "teleop") {
-              simulator.robot.attachedObj = null;
-          }
-
           if (e.data.firstCodeUpload !== undefined) {
             console.log("Field upload successful")
           }
       } catch(err) {
           console.log("Error uploading code. Please check syntax.");
       }
+      // Draw objects if no active simulation
+      if (simulator.mode == "idle") {
+          simulator.drawObjs();
+      }
 
+      // Objects get redefined right away in teleop mode.
+      // Attached objects get removed, so set to null.
+      if (simulator.mode == "teleop") {
+          simulator.robot.attachedObj = null;
+      }
     }
 
     if (e.data.start === true) {
